c78148b4d36c2f30e69a814c416b1addf5ca7bdf
myaso chel <chel.myaso@inbox.ru>
2020-11-07 10:27:24 +0800

    Initiaal migration added

---
+++ Data/Migrations/20201107022631_InitialMigration.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Velvetech.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Group",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Group", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sex",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(fixedLength: true, maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sex", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "(newid())"),
+                    Firstname = table.Column<string>(maxLength: 40, nullable: false),
+                    Middlename = table.Column<string>(maxLength: 60, nullable: true),
+                    Lastname = table.Column<string>(maxLength: 40, nullable: false),
+                    Callsign = table.Column<string>(maxLength: 16, nullable: true),
+                    SexId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Student_Student",
+                        column: x => x.SexId,
+                        principalTable: "Sex",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grouping",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(nullable: false),
+                    GroupId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grouping", x => new { x.StudentId, x.GroupId });
+                    table.ForeignKey(
+                        name: "FK_Grouping_Group_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Group",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Grouping_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grouping_GroupId",
+                table: "Grouping",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_Unique_Callsign",
+                table: "Student",
+                column: "Callsign",
+                unique: true,
+                filter: "[Callsign] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_SexId",
+                table: "Student",
+                column: "SexId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grouping");
+
+            migrationBuilder.DropTable(
+                name: "Group");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Sex");
+        }
+    }
+}
