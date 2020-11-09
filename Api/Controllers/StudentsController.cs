8929e0759217f56031df64cefa0337441ef0bd22
myaso chel <chel.myaso@inbox.ru>
2020-11-09 13:06:33 +0800

    API project created

---
+++ Api/Controllers/StudentsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Velvetech.Domain.Entities;
+using Velvetech.Domain.Entities.Validations;
+using Velvetech.Domain.Services.External.Interfaces;
+using Velvetech.Domain.Services.Internal.Interfaces;
+using Velvetech.Domain.Specifications;
+using Velvetech.Presentation.Shared;
+using Velvetech.Presentation.Shared.Dtos;
+using Velvetech.Presentation.Shared.Requests;
+
+namespace Velvetech.Api.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class StudentsController : ControllerBase
+	{
+		private readonly ICrudService<Student, Guid> _studentCrudService;
+		private readonly IStudentValidationService _studentValidationService;
+		private readonly IListService<Sex, int> _sexList;
+
+		public StudentsController(ICrudService<Student, Guid> studentCrudService,
+			IListService<Sex, int> sexList, IStudentValidationService studentValidationService)
+		{
+			_studentCrudService = studentCrudService;
+			_sexList = sexList;
+			_studentValidationService = studentValidationService;
+		}
+
+		// GET: api/Test/Students
+		[HttpGet]
+		public async Task<ActionResult<Page<StudentDto>>> ListAsync([FromQuery] StudentFilterPagedRequest request)
+		{
+			var pageSize = request.PageSize ?? 10;
+			var pageIndex = request.PageIndex ?? 0;
+
+			if (pageSize < 10)
+				pageSize = 10;
+
+			var totalItems = await _studentCrudService.CountAsync(
+				new StudentSpecification(
+					request.Sex,
+					request.Fullname,
+					request.Callsign,
+					request.Group));
+
+			var lastPageIndex = totalItems / pageSize;
+
+			if (totalItems == pageSize * lastPageIndex)
+				lastPageIndex--;
+
+			if (pageIndex > lastPageIndex)
+				pageIndex = lastPageIndex;
+
+			if (pageIndex < 0)
+				pageIndex = 0;
+
+			var filter = new StudentSpecification(
+				skip: pageSize * pageIndex,
+				take: pageSize,
+				sex: request.Sex,
+				fullname: request.Fullname,
+				callsign: request.Callsign,
+				@group: request.Group);
+
+			var students = await _studentCrudService.ListAsync(filter)
+				.Select(Extensions.ToDto)
+				.ToArrayAsync();
+
+			return new Page<StudentDto>
+			{
+				IsLastPage = pageIndex == lastPageIndex,
+				PageIndex = pageIndex,
+				PageSize = pageSize,
+				Items = students,
+			};
+		}
+
+		// GET: api/Test/ListIncluded
+		[HttpGet]
+		public async Task<ActionResult<StudentDto[]>> ListIncludedAsync([FromQuery] IncludedStudentsRequest request)
+		{
+			if (!request.GroupId.HasValue)
+				return BadRequest($"Corrupted or missing {nameof(request.GroupId)}");
+
+			var filter = new IncludedStudentsSpecification(request.GroupId.Value);
+			var students = await _studentCrudService.ListAsync(filter)
+				.Select(Extensions.ToDto)
+				.ToArrayAsync();
+
+			return students;
+		}
+
+		// GET: api/Test/ListIncluded
+		[HttpGet]
+		public async Task<ActionResult<StudentDto[]>> ListNotIncludedAsync([FromQuery] IncludedStudentsRequest request)
+		{
+			if (!request.GroupId.HasValue)
+				return BadRequest($"Corrupted or missing {nameof(request.GroupId)}");
+
+			var filter = new NotIncludedStudentsSpecification(request.GroupId.Value);
+			var students = await _studentCrudService.ListAsync(filter)
+				.Select(Extensions.ToDto)
+				.ToArrayAsync();
+
+			return students;
+		}
+
+		// GET: api/Test/Students
+		[HttpGet]
+		public async Task<ActionResult<SexDto[]>> SexListAsync() => await _sexList.ListAsync()
+				.Select(Extensions.ToDto)
+				.ToArrayAsync();
+
+		// GET: api/Test/Get
+		[HttpGet("{id}")]
+		public async Task<ActionResult<StudentDto>> GetAsync(Guid? id)
+		{
+			if (id is null)
+				return BadRequest("Corrupted Id");
+
+			return (await _studentCrudService.GetByIdAsync(id.Value)).ToDto();
+		}
+
+		// GET: api/Test/StudentsCount
+		[HttpGet]
+		public async Task<ActionResult<int>> StudentsCountAsync() => await _studentCrudService.CountAsync();
+
+		// PUT: api/Students/Add
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+		[HttpPost]
+		public async Task<ActionResult<StudentDto>> AddAsync(StudentDto dto)
+		{
+			var validator = new StudentValidator(_studentValidationService);
+			var entry = await Student.BuildAsync(validator, dto.SexId, dto.Firstname, dto.Middlename, dto.Lastname, dto.Callsign);
+
+			if (entry.HasValidationErrors)
+				return BadRequest(entry.ErrorsStrings);
+
+			entry = await _studentCrudService.AddAsync(entry);
+			return Ok(entry);
+		}
+
+		// PUT: api/Students
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+		[HttpPut]
+		public async Task<ActionResult<Student>> UpdateAsync(StudentDto dto)
+		{
+			var entry = await _studentCrudService.GetByIdAsync(dto.Id);
+			if (entry is null)
+				return NotFound();
+
+			var validator = new StudentValidator(_studentValidationService);
+			entry.SelectValidator(validator);
+
+			entry.SetFirstname(dto.Firstname);
+			entry.SetMiddlename(dto.Middlename);
+			entry.SetLastname(dto.Lastname);
+			await entry.SetCallsignAsync(dto.Callsign);
+			entry.SetSexId(dto.SexId);
+
+			if (entry.HasValidationErrors)
+				return BadRequest(entry.ErrorsStrings);
+
+			await _studentCrudService.UpdateAsync(entry);
+			return Ok(entry);
+		}
+
+		// DELETE: api/Students/Delete/{GUID}
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> DeleteAsync(Guid id)
+		{
+			await _studentCrudService.DeleteAsync(id);
+			return Ok();
+		}
+	}
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Velvetech.Data;
+using Velvetech.Domain.Common;
+using Velvetech.Domain.Entities;
+using Velvetech.Domain.Services.External;
+using Velvetech.Domain.Services.External.Interfaces;
+using Velvetech.Domain.Services.Internal;
+using Velvetech.Domain.Services.Internal.Interfaces;
+
+namespace Velvetech.Api
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			//.AddNewtonsoftJson(options => 
+			//	options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+			services.AddDbContext<AppDbContext>();
+
+			services.AddScoped(typeof(IAsyncRepository<,>), typeof(EfRepository<,>));
+			services.AddScoped(typeof(IListService<,>), typeof(ListService<,>));
+			services.AddScoped(typeof(ICrudService<Student, Guid>), typeof(StudentCrudService));
+			services.AddScoped(typeof(ICrudService<Group, Guid>), typeof(GroupCrudService));
+			services.AddScoped(typeof(IGroupingService), typeof(GroupingService));
+			services.AddScoped(typeof(IStudentValidationService), typeof(StudentValidationService));
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+				endpoints.MapControllers();
+				endpoints.MapFallbackToFile("index.html");
+			});
+		}
+	}
+}
