f9c620b879a00f3c8d6395c56e6a02e4857979c6
myaso chel <chel.myaso@inbox.ru>
2020-11-10 07:58:35 +0800

    Binaries path cleanup

--- Presentation/Shared/Dtos/SexDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Velvetech.Presentation.Shared.Dtos
-{
-	public class SexDto
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
-}
--- Presentation/Shared/Requests/IncludedStudentsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Velvetech.Presentation.Shared.Requests
-{
-	public class IncludedStudentsRequest
-	{
-		public Guid GroupId { get; set; }
-	}
-}
